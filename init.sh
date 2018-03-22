68f6d847c0bad15d5fa57eb68a2d970a4094841f
Patrik Forsberg <patrik.forsberg@ip-only.se>
2018-03-22 15:11:48 +0100

    Added credits and dynamic laravel home
    Signed-off-by: Patrik Forsberg <patrik.forsberg@ip-only.se>

--- init.sh
+++ init.sh
@@ -12,15 +12,21 @@ if [ -z "$LARAVEL_HORIZON" ]; then
 	LARAVEL_HORIZON=false
 fi
 
+if [ -z "$LARAVEL_HOME" ]; then
+    LARAVEL_HOME = "/var/www"
+fi
+
 if [ "$LARAVEL_HORIZON" = false ]; then
 	sed -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
 		-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
+        -e "s~%%WWWHOME%%~$LARAVEL_HOME~" \
 		/etc/supervisor/conf.d/laravel-worker.conf.tpl > /etc/supervisor/supervisord.conf
 fi
 
 if [ "$LARAVEL_HORIZON" = true ]; then
 	sed -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
 		-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
+        -e "s~%%WWWHOME%%~$LARAVEL_HOME~" \
 		/etc/supervisor/conf.d/laravel-horizon.conf.tpl > /etc/supervisor/supervisord.conf
 fi
 
